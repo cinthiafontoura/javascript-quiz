2f8076303133bcdedcf25db1b324655364c4ef45
Cinthia Fontoura <cinthiafontouras@gmail.com>
2022-05-07 14:06:54 +0100

    Add a function to go to the next question and atualize the game progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,6 +43,7 @@ const quizQuestion = [
 ];
 
 // Constants and vaviables to make the code easier to read
+const quiz = document.getElementById('quiz');
 const question = document.getElementById('quiz__question');
 const a_option = document.getElementById('a__option');
 const b_option = document.getElementById('b__option');
@@ -61,6 +62,9 @@ document.addEventListener('DOMContentLoaded', loadQuiz());
  * Get data from the quizQuestion constant and add to the game
  */
 function loadQuiz() {
+
+  document.getElementById('question_number').innerText = currentQuestion + 1; // Show how many questions are left
+  document.getElementById('total_question').innerText = quizQuestion.length; // Show how many questions the quiz have
   
   const currentQuestionData = quizQuestion[currentQuestion];
 
@@ -68,11 +72,11 @@ function loadQuiz() {
   a_option.innerText = currentQuestionData.a;
   b_option.innerText = currentQuestionData.b;
   c_option.innerText = currentQuestionData.c;
-  d_option.innerText = currentQuestionData.d;
-
-  checkAnswer();
+  d_option.innerText = currentQuestionData.d;    
 
 }
+
+checkAnswer();
 
 function checkAnswer() {
   for (let button of buttons) {
@@ -82,11 +86,27 @@ function checkAnswer() {
 
       if (answer === quizQuestion[currentQuestion].correct) {
         console.log('correct');
+        correctAnswer++;
+        nextQuestion();
+                
       } else {
         console.log('wrong choice');
-      }
-
-    });    
+        incorrectAnswer++;
+        nextQuestion();
+      }      
+    });        
   }
 }
 
+function nextQuestion() {
+  currentQuestion++;
+  if (currentQuestion < quizQuestion.length) {
+    loadQuiz();
+  } else {
+    quiz.innerHTML = `
+    <h2>Congratulations!</h2>
+    <h3> You answered ${correctAnswer}/${quizQuestion.length} questions correctly.</h3>
+    <button class="quiz__btn" onclick="location.reload()">Take the quiz again</button>
+    `;
+  }        
+}
